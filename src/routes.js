a4bbbb6719b120571b1ac64355ad964557671cad
PedroNogueir4 <pedroffn209@gmail.com>
2023-04-11 17:30:16 -0300

    Comunicação do model de usuário com banco de dados realizada

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import User from "./app/models/User";
+import { v4 } from "uuid";
 
 const routes = new Router();
 
 routes.get("/", (request, response) => {
-  return response.json({ message: "Hello Word" });
+
+  const user = User.create({
+    id: v4(),
+    name: "Pedro",
+    email: "pedroffn2@gmail.com",
+    password_hash: "12344fe",
+    admin:true
+
+  })
+
+  return response.json(user);
 });
 
 export default routes;
