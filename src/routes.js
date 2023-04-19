f5984406cbb7a6e9fa28c1aea027e3c8697b1116
PedroNogueir4 <pedroffn209@gmail.com>
2023-04-19 16:32:31 -0300

    Controller de User implementado

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,10 @@
 import { Router } from "express";
-import User from "./app/models/User";
-import { v4 } from "uuid";
+import UserController from "./app/controllers/UserController";
 
-const routes = new Router();
+const routes = new Router()
 
-routes.get("/", (request, response) => {
+routes.post('/users', UserController.store)
 
-  const user = User.create({
-    id: v4(),
-    name: "Pedro",
-    email: "pedroffn2@gmail.com",
-    password_hash: "12344fe",
-    admin:true
-
-  })
-
-  return response.json(user);
-});
+  
 
 export default routes;
