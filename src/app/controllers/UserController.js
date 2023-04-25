d041e52bca1407e7908d6646afbf66f44bb143bc
PedroNogueir4 <pedroffn209@gmail.com>
2023-04-25 15:43:46 -0300

    Validação de dados do usuário realizada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,25 @@
-
+import * as Yup from "yup"
 import User from "../models/User";
 
 class UserController {
     async store(request, response) {
 
+        const userSchema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean()
+        })
+
+        try {
+            await userSchema.validateSync(request.body, { abortEarly: false })
+        } catch (error) {
+            return response.status(400).json({ error: error.errors })
+        }
+
+
+
         const { name, email, password_hash, admin } = request.body
-        console.log(request.body)
 
         const user = await User.create({
             name,
@@ -14,7 +28,7 @@ class UserController {
             admin,
         })
 
-        return response.status(201).json(user)
+        return response.status(201).json({ id: user.id, name, email, admin })
     }
 }
 
